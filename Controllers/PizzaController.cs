04617727c1fe177c77c33046d60dc1b0ced80475
Timibreez <ffftimi@gmail.com>
2022-10-13 09:35:54 +0100

    Implemented Create, Delete and Update

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -15,6 +15,7 @@ public class PizzaController: ControllerBase{
     public ActionResult<List<Pizza>> GetAll() =>
         PizzaService.GetAll();
     
+    // Get Pizza by Id
     [HttpGet("{id}")]
     public ActionResult<Pizza> Get(int id){
         var pizza = PizzaService.Get(id);
@@ -23,5 +24,40 @@ public class PizzaController: ControllerBase{
             return NotFound();
         
         return pizza;
+    }
+
+    // Add Pizza to Pizza List
+    [HttpPost]
+    public IActionResult Create(Pizza pizza){
+        // saves new Pizza
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new {id = pizza.Id}, pizza);
+    }
+
+    // Update Pizza Information
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza){
+        if (id != pizza.Id)
+            return BadRequest();
+        
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+        
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    // Delete Pizza from Memory
+    [HttpDelete]
+    public IActionResult Delete(int id){
+        var pizza = PizzaService.Get(id);
+        if (pizza is null)
+            return NotFound();
+        
+        PizzaService.Delete(id);
+
+        return NoContent();
     }
 }
