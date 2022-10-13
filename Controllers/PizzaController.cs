0d1cb550d278a6f9dc221a38e032d83259cb4bd5
Timibreez <ffftimi@gmail.com>
2022-10-13 08:37:07 +0100

    added Get all and Get HTTP request to Pizza Controller

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -8,7 +8,20 @@ namespace Contosopizza.Controllers;
 [Route("[controller]")]
 
 public class PizzaController: ControllerBase{
-    public PizzaController(){
+    public PizzaController(){}
+
+    //Get all Pizza in Memory
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+        PizzaService.GetAll();
+    
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id){
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
         
+        return pizza;
     }
 }
