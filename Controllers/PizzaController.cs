0448f1188766967051bccf840dab5d7230c2a6ab
Timibreez <ffftimi@gmail.com>
2022-10-13 08:26:49 +0100

    created PizzaController in root Controller directory

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,14 @@
+using Contosopizza.Models;
+using Contosopizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contosopizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class PizzaController: ControllerBase{
+    public PizzaController(){
+        
+    }
+}
